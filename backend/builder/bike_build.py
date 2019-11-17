799347f0ab4ff375ded0ebb0c3544b787c9663e8
Wojciechowski Marcin <wojc.marcin@gmail.com>
2019-11-17 20:52:10 +0100

    #17: A bit better looking graph

---
+++ backend/builder/bike_build.py
@@ -0,0 +1,2 @@
+class BikeBuild:
+    pass
--- backend/builder/builder.py
+++ backend/builder/builder.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse
+from bikeparts.models import *
+from bikeproperties.models import *
 
 
 class Builder:
@@ -7,6 +9,9 @@ class Builder:
         self.budget = budget
         self.bike_type = bike_type
         self.bike_parts = bike_parts
+
+    def make_build(self):
+        pass
 
     def make_response(self):
         return JsonResponse(self.bike_parts)
